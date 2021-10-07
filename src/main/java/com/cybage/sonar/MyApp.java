404bff371f345a6374bddc20ffe5896fd35ddc6a
Astha Gangwar <asthag@cybage.com>
2021-10-07 11:13:44 +0530

    astha

---
+++ src/main/java/com/cybage/sonar/MyApp.java
@@ -0,0 +1,14 @@
+package com.cybage.sonar;
+
+/**
+ * Hello world!
+ *
+ */
+public class MyApp 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/cybage/sonar/MyAppWithBugs.java
@@ -0,0 +1,13 @@
+package com.cybage.sonar;
+
+/**
+ * Hello world!
+ *
+ */
+public class MyAppWithBugs 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
